fc3a321ab16a809ba9d3ee700ea3edc817a4ca14
AliGaxiola <ali.gaxiola@uabc.edu.mx>
2025-03-20 17:29:34 -0700

    Add Supabase integration and display roles example in Lector component

--- app/(main)/lector/page.tsx
+++ app/(main)/lector/page.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+"use client";
 
-function page() {
+import { useEffect, useState } from "react";
+import { supabase } from "@/lib/supabase";
+
+function Lector() {
+  const [roles, setRoles] = useState<{ id: string; nombre: string }[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const { data, error } = await supabase.from("roles").select("*");
+      if (error) console.error(error);
+      else setRoles(data);
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div>page</div>
-  )
+    <div>
+      <h1>Roles</h1>
+      <ul>
+        {roles.map((user) => (
+          <li key={user.id}>{user.nombre}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default page
+export default Lector;
